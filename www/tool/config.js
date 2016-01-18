d727399fe666852fc50ca415d1ba0fe3d3beaa38
LinJieLinLin <993353454@qq.com>
2016-01-18 11:20:47 +0800

    add tool

---
+++ www/tool/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    csslint: {
+        
+    },
+    jshint: {
+        
+    }
+};
---
+++ www/tool/gulp/watch.js
@@ -0,0 +1,10 @@
+// var browserSync = require('browser-sync');
+module.exports = function(gulp, G, dir) {
+    gulp.task('reload', function() {
+        // browserSync.reload();
+    });
+    gulp.task('watch', function() {
+        // gulp.watch(dir('src/styles/**/*.scss'), 
+        //     G.sync(gulp).sync(['sass', 'reload']));
+    });
+};
